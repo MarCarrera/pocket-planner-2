d0f095630e71b4a06afd3aa63a76898dfb5a11cc
MarCarrera <kmcarreraprieto@gmail.com>
2024-06-12 12:25:16 -0600

    creando vista de notificaciones

--- lib/utils/prueba.dart
+++ lib/utils/prueba.dart
@@ -14,19 +14,33 @@ class _PruebaState extends State<Prueba> {
     final idUser = ModalRoute.of(context)?.settings.arguments;
 
     return MaterialApp(
-        title: 'Material App',
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
           backgroundColor: Colors.grey.shade100,
           body: SafeArea(
-            child: Stack(
-              alignment: AlignmentDirectional.center,
-              children: [
-                background_container(context, idUser.toString()),
-                Positioned(
-                  top: 120,
-                  child: main_container(),
-                ),
-              ],
+            child: SingleChildScrollView(
+              child: Column(
+                //alignment: AlignmentDirectional.center,
+                children: [
+                  background_container(context, idUser.toString()),
+                  Column(
+                    children: [
+                      card_notification(),
+                      card_notification(),
+                      card_notification(),
+                      card_notification(),
+                      card_notification(),
+                      card_notification(),
+                      card_notification(),
+                      card_notification(),
+                      card_notification(),
+                      card_notification(),
+                      card_notification(),
+                      card_notification(),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ));
@@ -35,71 +49,142 @@ class _PruebaState extends State<Prueba> {
   Column background_container(BuildContext context, String idUser) {
     return Column(
       children: [
-        Container(
-          width: double.infinity,
-          height: 240,
-          decoration: const BoxDecoration(
-            color: Color(0xff368983),
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(20),
-              bottomRight: Radius.circular(20),
-            ),
-          ),
-          child: Column(
+        const SizedBox(height: 40),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const SizedBox(height: 40),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: const Icon(Icons.arrow_back, color: Colors.white),
-                    ),
-                    Text(
-                      idUser,
-                      style: GoogleFonts.fredoka(
-                          fontSize: 20, color: Colors.white),
-                    ),
-                    const Icon(
-                      Icons.attach_file_outlined,
-                      color: Colors.white,
-                    )
-                  ],
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Icon(
+                  Icons.arrow_back,
+                  color: Color(0xff368983),
                 ),
+              ),
+              Text(
+                'Notificaciones',
+                style: GoogleFonts.fredoka(
+                  fontSize: 20,
+                  color: Color(0xff368983),
+                ),
+              ),
+              const Icon(
+                Icons.attach_file_outlined,
+                color: Color(0xff368983),
               )
             ],
           ),
-        ),
+        )
       ],
     );
   }
 
-  Container main_container() {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.white,
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5), // Color de la sombra
-            spreadRadius: 6, // Cuánto se extiende la sombra
-            blurRadius: 8, // Qué tan desenfocado está el borde de la sombra
-            offset: Offset(3, 10), // Desplazamiento de la sombra en x y en y
+  Padding card_notification() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 15),
+      child: Dismissible(
+        key: Key('5'),
+        direction: DismissDirection.endToStart,
+        background: Container(
+          alignment: Alignment.centerRight,
+          padding: const EdgeInsets.only(right: 20.0),
+          color: Colors.red,
+          child: const Icon(
+            Icons.delete,
+            color: Colors.white,
           ),
-        ],
-      ),
-      height: MediaQuery.of(context).size.width * 1.2,
-      width: MediaQuery.of(context).size.width * 0.76,
-      child: Column(
-        children: [
-          const SizedBox(height: 50),
-          const SizedBox(height: 25),
-        ],
+        ),
+        onDismissed: (direction) async {
+          var idFinance = 5;
+          // Eliminar el elemento de la lista de datos
+          //finances.removeWhere((element) => element.idFinance == idFinance);
+          String idFinanceString = idFinance.toString();
+          //await eliminarFinanza(idFinance: idFinanceString);
+          // Esperar a que se muestre el diálogo y se cierre completamente
+          /*await ShowDelete().showDeleteDialog(context).then((_) {
+            // Llamar a setState para reconstruir la vista y mostrar los nuevos datos
+            // Verificar si el widget todavía está montado
+            if (mounted) {
+              // Llamar a setState solo si el widget está montado
+              setState(() {});
+            }
+          });*/
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(14),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5), // Color de la sombra
+                spreadRadius: 6, // Cuánto se extiende la sombra
+                blurRadius: 8, // Qué tan desenfocado está el borde de la sombra
+                offset:
+                    Offset(3, 10), // Desplazamiento de la sombra en x y en y
+              ),
+            ],
+          ),
+          height: MediaQuery.of(context).size.width * 0.24,
+          width: MediaQuery.of(context).size.width * 0.9,
+          child: Column(
+            children: [
+              const SizedBox(height: 15),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: Container(
+                            height: MediaQuery.of(context).size.width * 0.17,
+                            width: 2,
+                            color: Color(0xff368983),
+                          ),
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Titulo de notificacion',
+                              style: GoogleFonts.fredoka(
+                                fontSize: 22,
+                                color: Color(0xff368983),
+                              ),
+                            ),
+                            Text(
+                              'Descripción de notificacion',
+                              style: GoogleFonts.fredoka(
+                                fontSize: 19,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Text(
+                              '05 de Junio a las 14:30 pm',
+                              style: GoogleFonts.fredoka(
+                                fontSize: 17,
+                                color: Colors.grey.shade600,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Image.asset('assets/images/Todos.png', height: 84),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
       ),
     );
   }
